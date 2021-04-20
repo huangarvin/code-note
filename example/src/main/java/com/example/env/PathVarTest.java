2f5587f5ebb117f18b5d91c20edbaa90aeab82db
huangsuipiao <549755697@qq.com>
2021-04-20 16:48:46 +0800

    config test

---
+++ example/src/main/java/com/example/env/PathVarTest.java
@@ -0,0 +1,10 @@
+package com.example.env;
+
+/**
+ * @author YiTian (HuangSuip sp.huang@tuya.com)
+ */
+public class PathVarTest {
+  public static void main(String[] args) {
+    System.out.println(System.getenv("SONATYPE_NEXUS_USERNAME"));
+  }
+}
